505cc4a62cbefae92f4f1297ba9c5a5983e18ece
foreverbostick <brian.bostick@protonmail.com>
2024-03-17 08:27:45 -0400

    Updated variables to be more readable, cleaned up some code

--- main.py
+++ main.py
@@ -2,56 +2,48 @@ import sys
 import openpyxl as xl
 import datetime
 
-# Use sys argument to get path for new PTD/YTD report
+# PTD/YTD report
 path = sys.argv[1]
-#path = input('\n** ** What file do you want to open? (Use full path): ** **\n>> ')
-# Use 2nd sys argument to select the date
-argDate = sys.argv[2]
-#argDate = input('\n** ** What day of the month is the file for? (Don\'t include the month): ** **\n>> ')
-editDate = argDate
-# Path for the revenue summary spreadsheet
+# Date
+arg_date = sys.argv[2]
+edit_date = arg_date
+# Revenue Summary spreadsheet
 rs = sys.argv[3]
-#rs = input('\n** ** What file do you want to send the information to? (Use full path): ** **\n>> ')
 
 
-# Load the PTD/YTD into the program
+# Load the PTD/YTD
 ptdytd = xl.load_workbook(path)
-ptdSheet = ptdytd.active
+ptd_sheet = ptdytd.active
 
 # Identify the cells with the CC Allowances
-visa = ptdSheet['F36'].value
-mastercard = ptdSheet['F35'].value
-discover = ptdSheet['F34'].value
-amex = ptdSheet['F33'].value
+visa = ptd_sheet['F36'].value
+mastercard = ptd_sheet['F35'].value
+discover = ptd_sheet['F34'].value
+amex = ptd_sheet['F33'].value
 
-#
+####
 # Load the RS spreadsheet and make the CC Summary the active workbook
 #
-ccsWb = xl.load_workbook(rs)
-ccsSheet = ccsWb['Credit Card Summary']
-rsSheet = ccsWb['Revenue Summary']
+ccs_wb = xl.load_workbook(rs)
+ccs_sheet = ccs_wb['Credit Card Summary']
+rs_sheet = ccs_wb['Revenue Summary']
 
-#
 # Add 4 to the date variable, since the data starts on row 5
-#
-editDate = int(editDate) + 4
+edit_date = int(edit_date) + 4
 
-#
-# Row corresponding to the entered date
-#
 
-#
+####
 # Set variables for pasting locations
 #
 def ccsPaste():
-    visaPaste = ccsSheet.cell(row = editDate, column = 3)
-    visaPaste.value = visa
-    mastercardPaste = ccsSheet.cell(row = editDate, column = 4)
-    mastercardPaste.value = mastercard
-    discoverPaste = ccsSheet.cell(row = editDate, column = 5)
-    discoverPaste.value = discover
-    amexPaste = ccsSheet.cell(row = editDate, column = 10)
-    amexPaste.value = amex
+    visa_paste = ccs_sheet.cell(row = edit_date, column = 3)
+    visa_paste.value = visa
+    mastercard_paste = ccs_sheet.cell(row = edit_date, column = 4)
+    mastercard_paste.value = mastercard
+    discover_paste = ccs_sheet.cell(row = edit_date, column = 5)
+    discover_paste.value = discover
+    amex_paste = ccs_sheet.cell(row = edit_date, column = 10)
+    amex_paste.value = amex
 
 
 
@@ -61,35 +53,30 @@ def ccsPaste():
 #                                   #
 #####################################
 
-#
 # Variables for revenue summary
-#
-revenue = ptdSheet['F181'].value
-tax = ptdSheet['F192'].value
-misc = ptdSheet['F121'].value
-outlet = ptdSheet['F130'].value
-phone = ptdSheet['F157'].value
-cash = ptdSheet['F86'].value
-overshort = ptdSheet['F139'].value
+revenue = ptd_sheet['F181'].value
+tax = ptd_sheet['F192'].value
+misc = ptd_sheet['F121'].value
+outlet = ptd_sheet['F130'].value
+phone = ptd_sheet['F157'].value
+cash = ptd_sheet['F86'].value
+overshort = ptd_sheet['F139'].value
 
-#
-# Append RS info into RS spreadsheet
-#
 def rsPaste():
-    revenuePaste = rsSheet.cell(row = editDate, column = 3)
-    revenuePaste.value = revenue
-    taxPaste = rsSheet.cell(row = editDate, column = 4)
-    taxPaste.value = tax
-    miscPaste = rsSheet.cell(row = editDate, column = 5)
-    miscPaste.value = misc
-    outletPaste = rsSheet.cell(row = editDate, column = 6)
-    outletPaste.value = outlet
-    phonePaste = rsSheet.cell(row = editDate, column = 7)
-    phonePaste.value = phone
-    cashPaste = rsSheet.cell(row = editDate, column = 8)
-    cashPaste.value = cash
-    overshortPaste = rsSheet.cell(row = editDate, column = 10)
-    overshortPaste.value = overshort
+    revenue_paste = rs_sheet.cell(row = edit_date, column = 3)
+    revenue_paste.value = revenue
+    tax_paste = rs_sheet.cell(row = edit_date, column = 4)
+    tax_paste.value = tax
+    misc_paste = rs_sheet.cell(row = edit_date, column = 5)
+    misc_paste.value = misc
+    outlet_paste = rs_sheet.cell(row = edit_date, column = 6)
+    outlet_paste.value = outlet
+    phone_paste = rs_sheet.cell(row = edit_date, column = 7)
+    phone_paste.value = phone
+    cash_paste = rs_sheet.cell(row = edit_date, column = 8)
+    cash_paste.value = cash
+    overshort_paste = rs_sheet.cell(row = edit_date, column = 10)
+    overshort_paste.value = overshort
 
 
 #
@@ -98,9 +85,9 @@ def rsPaste():
 def eom():
     months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    thisMonth = datetime.date.today().month
-    eom = days[thisMonth - 1]
-    if eom == int(argDate):
+    this_month = datetime.date.today().month
+    eom = days[this_month - 1]
+    if eom == int(arg_date):
         print('\nIt looks like it\'s the end of the month. Don\'t forget to clean up your files!\n')
 
 
@@ -108,7 +95,7 @@ def eom():
 # Send the info to the RS spreadsheet
 #
 def wbSave():
-    ccsWb.save(filename=rs)
+    ccs_wb.save(filename=rs)
 
 #
 # Allow the user to cancel the script in case of an error
@@ -124,7 +111,7 @@ def cancel():
 # A little message that prints out to let you know the script is complete.
 # Will also let you know if there may have been an error with the date.
 def complete():
-    if str(argDate) == str(rsSheet.cell(row = editDate, column = 2).value) and str(ccsSheet.cell(row = editDate, column = 2).value):
+    if str(arg_date) == str(rs_sheet.cell(row = edit_date, column = 2).value) and str(ccs_sheet.cell(row = edit_date, column = 2).value):
         print('Done!')
     else:
         print('Something didn\'t add up, better check things out manually.')
@@ -133,7 +120,7 @@ def doubleCheck():
     print('\n ** ** Before we continue, here are the files you\'re working with: ** **\n')
     print(f'PTDYTD File: {path}\n')
     print(f'Destination File: {rs}\n')
-    print(f'\nDate: {argDate}\n')
+    print(f'\nDate: {arg_date}\n')
     print(f'\nVisa: {visa}\n')
     print(f'Mastercard: {mastercard}\n')
     print(f'Discover: {discover}\n')
